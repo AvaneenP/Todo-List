141abfae66fa4a9552e8ef2a7b1fbc87426c8abb
AvaneenP <avaneen@gmail.com>
2020-10-18 10:51:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,92 +1,136 @@
 import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
 
-function App() {
+  function App() {
+  const [todoList, setTodoList] = useState([]);
 
-const [name, setName] = useState("");
-const [description, setDescription] = useState("");
-const [dueDate, setDueDate] = useState("");
-const [showItem, setResult] = useState(false);
-const [items, setItems] = useState([]);
+  const addTodo = (value) => {
+    setTodoList([...todoList, value]);
+    console.log(todoList);
+  };
+
+  const deleteTodo = (value) => {
+    
+    let newArray = [];
+    
+    todoList.forEach((i) => {
+      if (i !== value) newArray.push(i);
+    });
+
+    setTodoList(newArray);
+    console.log(todoList);
+  };
 
   return (
     <div style={{textAlign: "center"}}>
-    <input style={{display: "flex", 
-  flexDirection: "column",
-  width: "300px",
-  marginLeft: "auto",
-  marginRight: "auto",
-  border: "1px solid red"}}
-     type="text" onChange={(event) => setName(event.target.value)} />
-    
-    <input style={{display: "flex", 
-  flexDirection: "column",
-  width: "300px",
-  marginLeft: "auto",
-  marginRight: "auto",
-  border: "1px solid blue",
-  marginTop: "20px"}}
-     type="text"  onChange={(event) => setDescription(event.target.value)} />
-    
-    <input style={{display: "flex", 
-  flexDirection: "column",
-  width: "300px",
-  marginLeft: "auto",
-  marginRight: "auto",
-  border: "1px solid green",
-  marginTop: "20px"}}
-    type="text"  onChange={(event) => setDueDate(event.target.value)} />
-
-<Button style={{marginTop:"15px", 
-display: "flex", 
-flexDirection: "column", 
-marginLeft: "auto", 
-marginRight: "auto"}}
-onClick={() => setResult(true) }
-variant="contained"
-size="large"
-color="primary">
-    Add Item!</Button>
-
-    { (name && description && dueDate) && showItem && <ToDoCard
-    todoItem = {name}
-    description = {description}
-    dueDate = {dueDate}/> }
+      <h1>To-do List App</h1>
+      
+      <InputField addTodo = {addTodo}/>
+        
+        {todoList.map(todo => 
+          <Todo 
+            title = {todo.title}
+            description = {todo.description}
+            date = {todo.date}
+          />
+        )}
+​
     </div>
-  )
-
+  );
 }
 
+export default App;
 
-const ToDoCard = ( {todoItem, description, dueDate} ) => {
 
+function Todo({ title, description, date }) {
   const [cross, setCross] = useState(false);
-  const [deleteItem, setStatus] = useState(true);
 
-  return deleteItem && <div style={{ background: "#899ea3",
+  return (
+    <div style={{ backgroundColor: "whitesmoke",
+      display: "flex", 
+      flexDirection: "column",
+      alignItems: "center",
+      width: "500px",
+      marginLeft: "auto",
+      marginRight: "auto",
+      border: "3px solid black",
+      marginTop: "30px",
+      textDecoration: cross ? "line-through" : "none"
+       }}>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <p>{date}</p>
+        
+        <Button style={{marginTop:"auto"}}
+        onClick={ () => setCross(!cross) } 
+        variant="contained"
+        size="medium"
+        color="primary"
+        >Complete!</Button>
+
+        <Button style={{marginTop:"15px"}}
+        variant="contained"
+        size="medium"
+        color="primary">Delete</Button>
+        
+    </div>
+  );
+}
+
+function InputField({ addTodo }) {
+
+  const [value, setValue] = useState({ title: "", description: "", date: "" });
+  
+  const inputStyle = {
   display: "flex", 
   flexDirection: "column",
-  alignItems: "center",
-  width: "500px",
+  width: "300px",
   marginLeft: "auto",
   marginRight: "auto",
-  border: "3px solid black",
-  marginTop: "30px",
-  textDecoration: cross ? "line-through" : "none"
- }} >
-    <h3>{todoItem}</h3>
-    <p>{description}</p>
-    <h4>{dueDate}</h4>
-    <button onClick={ () => setCross(!cross) } >Complete!</button>
+  };
 
-    <button style={{marginTop:"15px", 
-    display: "flex", 
-    flexDirection: "column",
-    marginLeft: "auto", 
-    marginRight: "auto"}}
-    onClick={() => setStatus(false)}>
-    Delete Item!</button>
+  const handleChange = (e) => {
+    setValue({
+      ...value,
+      [e.target.id]: e.target.value
+    });
+  };
 
-  </div>
+  return (
+    <div style = { inputStyle }>
+      <input 
+        style={{ borderWidth: '0', backgroundColor: 'whitesmoke' }} 
+        placeholder='Enter title'
+        value = { value.title }
+        onChange = {e => handleChange(e)}
+        id='title'
+        >
+      </input>
+      <textarea 
+        style={{ borderWidth: '0', backgroundColor: 'whitesmoke' }}
+        placeholder='Enter description'
+        value = { value.description }
+        onChange = {e => handleChange(e)}
+        id='description'
+        >
+      </textarea>
+      <input 
+        style={{ borderWidth: '0', backgroundColor: 'whitesmoke' }} 
+        type='date' 
+        placeholder='Enter date'
+        value = { value.date }
+        onChange = {e => handleChange(e)}
+        id='date'
+        >
+      </input>
+
+      <Button 
+      style={{marginTop: "20px"}}
+      onClick={() => addTodo(value)}
+      variant="contained"
+        size="large"
+        color="primary">Add Item</Button>
+
+    </div>
+  );
 }
-export default App;
